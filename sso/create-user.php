be441440ada629b0b5a48bf5933176d9085b923d
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-16 13:46:32 +0100

    Implemented user creation.

---
+++ sso/create-user.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$DEBUG = $_GET["DEBUG"];
+if ($DEBUG) {
+    echo "<p>In debug mode.</p>";
+    print_r($_POST);
+}
+
+if (!(array_key_exists("username", $_POST) && array_key_exists("email", $_POST) && array_key_exists("password", $_POST) && array_key_exists("password2", $_POST) && array_key_exists("fullName", $_POST))) {
+    die("{\"status\": \"fail\", \"message\": \"Invalid request.\"}");
+}
+else if ($_POST["username"] == "" || $_POST["email"] == "" || $_POST["password"] == "" || $_POST["password2"] == "" || $_POST["fullName"] == "") {
+    die("{\"status\": \"fail\", \"message\": \"Please do not leave any fields blank.\"}");
+}
+else if ($_POST["password"] != $_POST["password2"]) {
+    die("{\"status\": \"fail\", \"message\": \"Passwords don't match.\"}");
+}
+
+$DB_server = "localhost:3306";
+$DB_user = "sso";
+$DB_pass = "";
+$DB_schema = "SSO";
+
+$conn = new mysqli($DB_server, $DB_user, $DB_pass, $DB_schema);
+
+if ($conn->connect_error) {
+    die("{\"status\": \"fail\", \"message\": \"Failed to connect to the database: " . $conn->connect_error . "\"}");
+}
+
+$stmt = $conn->prepare("INSERT INTO Users (username, email, fullName, password) VALUES (?, ?, ?, SHA2(?, 256))");
+$stmt->bind_param("ssss", $_POST["username"], $_POST["email"], $_POST["fullName"], $_POST["password"]);
+$state = $stmt->execute();
+
+if ($state) {
+    echo "{\"status\": \"success\"}";
+}
+else {
+    echo "{\"status\": \"fail\", \"error\": \"" . $stmt->error . "\"}";
+}
+
+$conn->close();
+?>
--- sso/js/sso.js
+++ sso/js/sso.js
@@ -1,4 +1,4 @@
-async function sso_try_login(username, password, callback) {
+function sso_try_login(username, password, callback) {
 
     let fd = new FormData();
     fd.set("username", username);
@@ -13,4 +13,19 @@ async function sso_try_login(username, password, callback) {
     .catch((error) => {
         console.log("SSO Request failed: " + error);
     });
+}
+
+function sso_create_new_user(username, email, password, password2, fullName, callback) {
+    let fd = new FormData();
+    fd.set("username", username);
+    fd.set("email", email);
+    fd.set("password", password);
+    fd.set("password2", password2);
+    fd.set("fullName", fullName);
+    fetch("https://data.nathcat.net/sso/create-user.php", {
+        method: "POST",
+        body: fd
+    })
+    .then((r) => response.json())
+    .then(callback);
 }
