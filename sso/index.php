e6b9a5f7ce498c4c282965919bc71bad2ba92a3a
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-16 15:20:22 +0100

    SSO Page simply copied from nathcat.net

--- sso/index.php
+++ sso/index.php
@@ -8,16 +8,36 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script src="https://data.nathcat.net/sso/js/sso.js"></script>
+        <script src="js/sso.js"></script>
     </head>
 
     <body>
         <div class="content">
-            <?php include("header.php"); ?>
+            <?php include("../header.php"); ?>
 
-            <div class="main"></div>
+            <div class="main align-center">
+                <?php 
+                session_start();
 
-            <?php include("footer.php"); ?>
+                if (array_key_exists("login-error", $_SESSION)) {
+                    echo "<div class='error-card'><h2>Login failed</h2><p>" . $_SESSION["login-error"] . "</p></div>";
+                }
+
+                if (array_key_exists("user", $_SESSION)) { 
+                    include("user-view.php"); 
+                } 
+                else { 
+                    if (!array_key_exists("newUser", $_GET)) { 
+                        include("login-form.php"); 
+                    } 
+                    else {
+                        include("new-user-form.php");
+                    }
+                }
+                ?>
+            </div>
+
+            <?php include("../footer.php"); ?>
         </div>
     </body>
 </html>
