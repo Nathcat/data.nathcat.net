d38aa6d6c42f25762299efb5004c34dca38b68af
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-16 11:50:54 +0100

    Change to use form data to pass the login data through to the SSO API.

--- sso/js/sso.js
+++ sso/js/sso.js
@@ -1,13 +1,15 @@
 async function sso_try_login(username, password, callback) {
+
+    let fd = new FormData();
+    fd.set("username", username);
+    fd.set("password", password);
+
     fetch("https://data.nathcat.net/sso/try-login.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({
-            "username": username,
-            "password": password
-        })
+        body: fd
     })
     .then((response) => response.json())
     .then(callback);
