3783a3b6016d1d9ebe5cd156f0ebe9c4e0289254
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-16 15:15:57 +0100

    Started creating basic SSO page

---
+++ sso/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AuthCat</title>
+
+        <link rel="stylesheet" href="https://nathcat.net/static/css/new-common.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://data.nathcat.net/sso/js/sso.js"></script>
+    </head>
+
+    <body>
+        <div class="content">
+            <?php include("header.php"); ?>
+
+            <div class="main"></div>
+
+            <?php include("footer.php"); ?>
+        </div>
+    </body>
+</html>
