2b7a95bf10065605960cffe63a6969eea461d30f
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-17 15:54:08 +0100

    Debugging cookies

--- sso/index.php
+++ sso/index.php
@@ -17,6 +17,10 @@
 
             <div class="main align-center">
                 <?php 
+                echo session_name();
+                session_start();
+                print_r($_SESSION);
+                
                 $session_name = session_name("AuthCat-SSO");
                 session_set_cookie_params(0, '/', $_SERVER["SERVER_NAME"]);
                 session_start();
