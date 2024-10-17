233e6af6d768a8d8696c23ee8b87b532342293e8
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-17 14:57:57 +0100

    Added .gitignore for user uploaded profile pictures, made local development possible, and implemented profile picture uploading.

--- sso/logout.php
+++ sso/logout.php
@@ -1,4 +1,4 @@
 <?php
 $session_name = session_name("AuthCat-SSO");
-session_set_cookie_params(0, '/', 'nathcat.net'); 
+session_set_cookie_params(0, '/', $_SERVER["SERVER_NAME"]); 
 session_start(); session_destroy(); ?>
--- sso/user-view.php
+++ sso/user-view.php
@@ -1,8 +1,44 @@
-<h1>Welcome, <?php echo $_SESSION["user"]["fullName"] ?>.</h1>
+<?php 
+if (isset($_POST["uploadPFP"])) {
+    $new_file = "../pfps/" . $_SESSION["user"]["id"] . ".png";
+    
+    if (getimagesize($_FILES["uploadFilePFP"]["tmp_name"]) !== false) {  // This checks if the file is a valid image
+        if (move_uploaded_file($_FILES["uploadFilePFP"]["tmp_name"], $new_file)) {
+            echo "<div class='content-card'><h2>Uploaded profile picture</h2></div>";
+            
+            try {
+                $conn = new mysqli("localhost:3306", "sso", "", "SSO");
+                $stmt = $conn->prepare("UPDATE Users SET pfpPath = ? WHERE id = ?");
+                $new_file_name = $_SESSION["user"]["id"] . ".png";
+                $stmt->bind_param("sd", $new_file_name, $_SESSION["user"]["id"]);
+                $stmt->execute();
+                $conn->close();
+
+                $_SESSION["user"]["pfpPath"] = $new_file_name;
+            } catch (Exception $e) {
+                echo "<div class='error-card'><h2>Failed to upload profile picture!</h2><p>" . $e->getMessage() . "</p></div>";
+            }
+        }
+        else {
+            echo "<div class='error-card'><h2>Failed to upload profile picture!</h2></div>";
+        }
+    }
+    else {
+        echo "<div class='error-card'><h2>Please select an image file!</h2></div>";
+    }
+}
+?>
+
+<h1>Welcome, <?php echo $_SESSION["user"]["fullName"]; ?>.</h1>
 
 <div class="profile-picture">
-    <img src="<?php echo "https://data.nathcat.net/pfps/" . $_SESSION["user"]["pfpPath"]; ?>">
+    <img src="<?php echo "/pfps/" . $_SESSION["user"]["pfpPath"]; ?>">
 </div>
+
+<form class="row align-center" method="POST" enctype="multipart/form-data">
+    <input type="file" name="uploadFilePFP" />
+    <input type="submit" name="uploadPFP" value="Upload new profile picture" />
+</form>
 
 <div class="content-card">
     <h2>User information</h2>
