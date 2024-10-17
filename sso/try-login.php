98c4602ecb6498016ecf6cf9e2a9ee94c0338f15
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-17 15:43:05 +0100

    nathcat.net domain cookie persistence

--- sso/try-login.php
+++ sso/try-login.php
@@ -1,6 +1,6 @@
 <?php
-$session_name = session_name("AuthCat-SSO");
-session_set_cookie_params(0, '/', $_SERVER["SERVER_NAME"]);
+session_name("AuthCat-SSO");
+session_set_cookie_params(0, '/', ".nathcat.net");
 session_start();
 
 header("Content-Type: application/json");
