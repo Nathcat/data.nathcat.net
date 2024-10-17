69ed18cd5b65cbbcec3c57ddf0a8999a93cbe298
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-17 01:04:17 +0100

    Add profile picture to user view page

--- sso/user-view.php
+++ sso/user-view.php
@@ -1,5 +1,13 @@
+<?php
+$session_name = session_name("AuthCat-SSO");
+session_set_cookie_params(0, '/', 'nathcat.net');
+session_start();
+?>
+
 <h1>Welcome, <?php echo $_SESSION["user"]["fullName"] ?>.</h1>
 
+<img class="large-circle-photo" src="<?php echo "https://data.nathcat.net/pfps/" . $_SESSION["user"]["pfpPath"]; ?>">
+                
 <div class="content-card">
     <h2>User information</h2>
     <p>Username: <?php echo $_SESSION["user"]["username"] ?></h1></p>
