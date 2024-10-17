4c951c6e26e2e42213e9c07d6a0279a553b57639
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-17 02:24:06 +0100

    Profile picture upload system attempt

--- sso/user-view.php
+++ sso/user-view.php
@@ -1,8 +1,33 @@
+<?php 
+if (isset($_POST["uploadPFP"])) {
+    $filename = $_FILES["uploadPFPFile"]["name"];
+    $tempname = $_FILES["uploadPFPFile"]["tmp_name"];
+    $folder = "../pfps/" . $filename;
+
+    if (move_uploaded_file($tempname, $folder)) {
+        $conn = new mysqli("localhost", "sso", "", "SSO");
+        $stmt = $conn->prepare("UPDATE Users SET pfpPath = ? WHERE id = ?");
+        $stmt->bind_param("si", $filename, $_SESSION["user"]["id"]);
+        $stmt->execute();
+        $conn->close();
+        echo "<div class='content-card'><h2>Uploaded profile picture.</h2></div>";
+    }
+    else {
+        echo "<div class='error-card'><h2>Failed to upload profile picture!</h2></div>";
+    }
+}
+?>
+
 <h1>Welcome, <?php echo $_SESSION["user"]["fullName"] ?>.</h1>
 
 <div class="profile-picture">
     <img src="<?php echo "https://data.nathcat.net/pfps/" . $_SESSION["user"]["pfpPath"]; ?>">
 </div>
+
+<form class="row" method="POST" action="">
+    <input type="file" name="uploadPFPFile" />
+    <input type="submit" name="uploadPFP" />
+</form>
 
 <div class="content-card">
     <h2>User information</h2>
