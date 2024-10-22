a10b762bf481727e835c29fdb90aa1d1843a01ab
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-10-22 21:51:46 +0100

    Created privacy policy document

---
+++ sso/docs/policies/privacy-policy.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AuthCat - Privacy Policy</title>
+
+        <link rel="stylesheet" href="https://nathcat.net/static/css/new-common.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div class="content">
+            <?php include("../../header.php"); ?>
+
+            <div class="main align-center">
+                <h1>AuthCat Privacy Policy</h1>
+
+                <p><i>AuthCat © Nathcat 2024</i></p>
+                <p>Contact <a href="mailto:nathan.baines@nathcat.net">nathan.baines@nathcat.net</a> for any queries.</p>
+                
+                <div class="content-card">
+                    <h2>What data does AuthCat store about you?</h2>
+                    <p>
+                        AuthCat stores the following information about its' users:
+                    </p>
+                    <ul>
+                        <li>Your email address</li>
+                        <li>Your full name</li>
+                        <li>Your <i>current</i> profile picture</li>
+                    </ul>
+                </div>
+
+                <div class="content-card">
+                    <h2>How will this data be used?</h2>
+                    <p>
+                        The data you provide us will be used to personalise your experience
+                        throughout <a href="https://nathcat.net">nathcat.net</a> and its'
+                        associated services.
+                    </p>
+                    <p>
+                        Your email may be used for communications relevant to your account
+                        and the aforementioned services.
+                    </p>
+                </div>
+
+                <div class="content-card">
+                    <h2>How accessible is this data?</h2>
+                    <p>
+                        The data stored by AuthCat is only accessible to members of the
+                        Nathcat development team <i>Who are given access to its associated databases</i>.
+                    </p>
+                    <p>
+                        <i><b>
+                            Note that this statement does NOT include profile pictures, which are
+                            accessible over the internet (if you have the appropriate link).
+                        </b></i>
+                    </p>
+                    <p>
+                        Further to this point, when searching for other users through the platform, some
+                        of your account information will be displayed in search results. The information
+                        which is shared is as follows:
+                    </p>
+                    <ul>
+                        <li>Your username</li>
+                        <li>Your full name</li>
+                        <li>Your profile picture</li>
+                    </ul>
+                </div>
+
+                <div class="content-card">
+                    <h2>Where is this data shared?</h2>
+                    <p>
+                        This data is shared throughout services on the Nathcat network.
+                    </p>
+                    <p>
+                        Your data will not be knowingly distributed by anyone with appropriate
+                        access, to any third party without your consent.
+                    </p>
+                </div>
+            </div>
+
+            <?php include("../../footer.php"); ?>
+        </div>
+    </body>
+</html>
