4d82fe9f4fc51919b97b888ccbd76a2295224e60
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-04 17:03:11 +0000

    Added CORS headers

--- sso/get-session.php
+++ sso/get-session.php
@@ -2,6 +2,10 @@
 include("start-session.php");
 
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: https://nathcat.net:8080");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
 echo json_encode($_SESSION);
 ?>
