89acddf29dd3fb40f3fc1f988856f98e5fd95824
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-04 16:40:25 +0000

    Added allow credentials header

--- sso/get-session.php
+++ sso/get-session.php
@@ -5,6 +5,7 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
 echo json_encode($_SESSION);
 ?>
