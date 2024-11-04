541249d8e8a65eb73b0b291437b13848de504121
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-04 16:12:20 +0000

    Set headers on session script

--- sso/get-session.php
+++ sso/get-session.php
@@ -1,5 +1,10 @@
 <?php
 include("start-session.php");
 
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 echo json_encode($_SESSION);
 ?>
