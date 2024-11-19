bf48ac60c2b8c16e59a78766e0941dbbef530d21
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-19 18:15:03 +0000

    Added debug header to `add-solved.php`

--- sudoku/add-solved.php
+++ sudoku/add-solved.php
@@ -16,7 +16,7 @@ if (!array_key_exists("HAS_SOLVED_SUDOKU", $_SESSION)) {
     die("{\"status\": \"fail\", \"message\": \"You have not just solved a sudoku.\"}");
 }
 
-if ($_SERVER["CONTENT_TYPE"] == "text/plain") {
+if ($_SERVER["CONTENT_TYPE"] == "text/plain" || array_key_exists("DEBUG", $_GET)) {
     if (array_key_exists("DEBUG", $_GET)) {
         $_PUZZLE = "2 8 4 6 9 5 7 3 1\n6 9 3 1 2 7 8 5 4\n7 1 5 8 4 3 2 9 6\n8 3 1 7 6 2 5 4 9\n9 7 2 5 3 4 6 1 8\n4 5 6 9 8 1 3 7 2\n1 2 9 3 5 6 4 8 7\n5 6 8 4 7 9 1 2 3\n3 4 7 2 1 8 9 6 5\n";
     }
