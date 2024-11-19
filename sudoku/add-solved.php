564b66e51eda3b15dbaab334f7a6272ecaad45bb
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-19 17:31:45 +0000

    Added sudoku utils and script to increase the puzzles solved counter by 1

---
+++ sudoku/add-solved.php
@@ -0,0 +1,49 @@
+<?php
+include("../sso/start-session.php");
+include("sudoku-utils.php");
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if (!array_key_exists("user", $_SESSION)) {
+    die("{\"status\": \"fail\", \"message\": \"Not logged in.\"}");
+}
+
+if ($_SERVER["CONTENT_TYPE"] == "text/plain") {
+    $_PUZZLE = file_get_contents("php://input");
+    $_PUZZLE = explode("\n", $_PUZZLE);
+
+    // Process the puzzle string into a 2D array
+    $_PUZZLE = array_map(function($v) {
+        return array_map(function($x) {
+            return intval($x);
+        }, explode(" ", $v));
+    }, $_PUZZLE);
+}
+else {
+    die("{\"status\": \"fail\", \"message\": \"May only pass text/plain with body as Sudoku data.\"}");
+}
+
+if (!is_solved($_PUZZLE)) {
+    die("{\"status\": \"fail\", \"message\": \"Submitted puzzle is not solved!\"}");
+}
+
+$conn = new mysqli("localhost:3306", "Sudoku", "", "Sudoku");
+
+if ($conn->connect_error) {
+    die("{\"status\": \"fail\", \"message\": \"Failed to connect to the database: " . $conn->connect_error . "\"}");
+}
+
+$stmt = $conn->prepare("INSERT INTO PuzzlesSolved (id) VALUES (?)");
+$stmt->bind_param("i", $_SESSION["user"]["id"]);
+$stmt->execute(); $stmt->close();
+
+$stmt = $conn->prepare("UPDATE PuzzlesSolved SET count = count + 1 WHERE id = ?");
+$stmt->bind_param("i", $_SESSION["user"]["id"]);
+$stmt->execute(); $stmt->close();
+
+$conn->close();
+
+echo "{\"status\": \"success\"}";
+?>
