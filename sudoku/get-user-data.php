c4e468c772fc8c699600c7a960e12aabf3f1f041
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-11-20 15:40:10 +0000

    Added `sudoku/get-user-data.php`

---
+++ sudoku/get-user-data.php
@@ -0,0 +1,20 @@
+<?php
+include("../sso/start-session.php");
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: https://sudoku.nathcat.net");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+
+if (!array_key_exists("user", $_SESSION)) {
+    die("{\"status\": \"fail\", \"message\": \"Not logged in.\"}");
+}
+
+$conn = new mysqli("localhost:3306", "Sudoku", "", "Sudoku");
+$stmt = $conn->prepare("SELECT count as `puzzles-solved` FROM PuzzlesSolved WHERE id = ?");
+$stmt->bind_param("i", $_SESSION["user"]["id"]);
+$stmt->execute(); $res = $stmt->get_result()->fetch_assoc();
+
+echo json_encode($res);
+?>
