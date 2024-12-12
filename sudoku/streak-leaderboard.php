66f11e48a4debfb11b2d1ced54e762ab76967c84
Nathcat <74734054+Nathcat@users.noreply.github.com>
2024-12-12 19:48:35 +0000

    Added streak leaderboard script

---
+++ sudoku/streak-leaderboard.php
@@ -0,0 +1,21 @@
+<?php 
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+
+$conn = new mysqli("localhost:3306", "Sudoku", "", "Sudoku");
+
+if ($conn->connect_error) {
+    die("{\"status\": \"fail\", \"message\": \"Failed to connect to the database: " . $conn->connect_error . "\"}");
+}
+
+$stmt = $conn->prepare("SELECT SSO.Users.username, SSO.Users.fullName, SSO.Users.pfpPath, UserData.puzzlesSolved, UserData.streakLength FROM UserData JOIN SSO.Users ON SSO.Users.id = UserData.id WHERE UserData.streakLength != 0 ORDER BY UserData.streakLength DESC LIMIT 5;");
+$stmt->execute(); $set = $stmt->get_result();
+$res = [];
+
+while ($r = $set->fetch_assoc()) {
+    array_push($res, $r);
+}
+
+echo json_encode($res);
+?>
