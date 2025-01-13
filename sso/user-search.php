3c80fd2b30d5ccbf91f3cd532bef0683443dd5f0
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-01-13 18:56:18 +0000

    Changed origin allow header in user search

--- sso/user-search.php
+++ sso/user-search.php
@@ -2,7 +2,7 @@
 include("start-session.php");
 
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
